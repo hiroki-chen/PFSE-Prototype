f1922226455ce5961e37efd08b1dba3abd4d921d
Hiroki Chen <etyyuiope@qq.com>
2023-02-23 10:09:05 +0000

    Update python script

--- eval/eval.py
+++ eval/eval.py
@@ -3,22 +3,29 @@ import os
 import subprocess
 
 
-def test(round, size, log_level, name):
+def test(type, round, size, log_level, name):
     env = os.environ.copy()
     env['RUST_LOG'] = log_level
     env['PATH'] = '/home/chb/.cargo/bin/:' + env['PATH']
 
-    columns = ['add_to_cart_order', 'order_hour_of_day',
-               'order_dow', 'AGEP', 'SPORDER', 'CIT', 'HICOV']
+    columns = ['order_number', 'add_to_cart_order', 'order_hour_of_day',
+               'reordered', 'AGEP', 'ST', 'SPORDER', 'SEX']
     if name in columns:
         columns = [name]
 
     for column in columns:
-        base = 'query_' + column + '.toml'
-        input_file = './test_suites/' + base
+        if type == 'query':
+            base = 'query_' + column + '.toml'
+            input_file = './test_suites/' + base
+        elif type == 'attack':
+            base = 'attack_' + column + '.toml'
+            input_file = './test_suites/others/' + base
+        else:
+            raise Exception('Unrecognized option')
+
         output_file = './data/' + base
-        command = 'cargo run --release -- -e perf -r {} -s {} -c {} -o {}'.format(
-            round, size, input_file, output_file)
+        command = 'cargo run --release -- -e {} -r {} -s {} -c {} -o {}'.format(type,
+                                                                                round, size, input_file, output_file)
 
         subprocess.run(command.split(), env=env)
 
@@ -33,6 +40,8 @@ if __name__ == '__main__':
                         help='The log level for `RUST_LOG`')
     parser.add_argument('-n', '--name', default='all',
                         help='Which test suites you want to run')
+    parser.add_argument('-t', '--type', default='perf',
+                        help='Perf or attack')
     args = parser.parse_args()
 
-    test(args.round, args.size, args.log_level, args.name)
+    test(args.type, args.round, args.size, args.log_level, args.name)
